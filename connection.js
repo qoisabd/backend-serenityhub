675ee178f3964cc37d65b178d0d8c68d9f47ab4e
Qois Abdul Qudus <qoisabdulquduss@gmail.com>
2024-07-23 18:07:25 +0700

    Backend-SerenityHub

---
+++ connection.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const config = require('./src/config');
+const {
+  dbHost,
+  dbName,
+  dbPassword,
+  dbPort,
+  dbUser,
+  maUser,
+  maPassword,
+  maServer,
+} = config;
+
+mongoose.set('strictQuery', false);
+const mongodb = `mongodb://${maUser}:${maPassword}@${maServer}`;
+
+const localConnection = `mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+
+mongoose.connect(mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', async () => {
+  console.log('database connect');
+});
+
+module.exports = db;
